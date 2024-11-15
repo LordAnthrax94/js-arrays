3fddd3567c325645a5b5a92732887adfb134dad6
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-15 17:38:44 +0100

    fix: completati esercizi pagina bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,14 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
+const reversedTeachers = [];
 
-for(let i = 0; 1 < teachers.length; 1--){
-
+for(let i = 0; i < teachers.length; i++){
+  reversedTeachers.unshift(teachers[i])
 }
+
+console.log(reversedTeachers);
+
 
 
 
@@ -24,29 +27,27 @@ const longNames = [];
 
 for (let i = 0; i < teachers.length; i++){
 
-  // controllare che ogni elemento dell'array sia composto da 5 o più caratteri
+    // controllare che ogni elemento dell'array sia composto da 5 o più caratteri
     //prendere l'elemento corrente
     //controllo che l'elemento siadi lunghezza >= di 5 caratteri
-      //Se è >= di 5 caratteri lo inserisco nell'array
-if(teachers[i] > 5 == longNames){
-  console.log(longNames);
+    //Se è >= di 5 caratteri lo inserisco nell'array
+  if(teachers[i].length >= 5){ 
+    longNames.push(teachers[i]);  
+  }  
 }
 
+console.log(longNames);
 
 
-  // teachers[i]
-
-  // if(teachers % 5 == 0){
-  //   console.log(teachers);
-    
-  // }
-  
-}
 
 // 3. Rimuovi 'Ed' dall'array teachers
-const removedTeacher = teachers.splice(1, 1);
-console.log(teachers);
+for (let i = 0; i < teachers.length; i++){
+  if(teachers[i] == 'Ed'){
+    teachers.splice(i, 1);   
+  }
+}
 
+console.log(teachers);
 
 
 
